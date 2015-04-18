833608e314a2e1dc058d07db6616f52cf643ea83
Louis Jenkins <theif517@gmail.com>
2015-04-17 20:28:44 -0400

    Me toying around with creating a simple Garbage Collector for my String_Utils, all thanks to: http://journal.stuffwithstuff.com/2013/12/08/babys-first-garbage-collector/
    It was interesting to work on, and I vaguely have a  better
    understanding of garbage collection, but no where near enough to
    implement a simple one, even for my own String_Utils, specializing
    solely on Strings. My initial expectations were the following:
    
    "Oh, cool, I use a garbage collector in the case that I don't want to
    end up worrying about allocating and deallocating memory all over the
    place!" but I walked away with less than expected.
    
    My thoughts on it though, it's neat, but it won't work for my program
    as it is, I'd have to give more thought as to how I can tailor it to
    my program. It uses a stack and a linked-list of objects which holds
    the string's value. Now, this sounds great, in fact, it's wonderful,
    because due to mark and sweep, it will free anything not on the
    stack... the bad part is that it's on a stack. Lets say I want to
    return a copy of a string? Guess what, I have to push the temporary
    string on the stack, then pop it off at the end of the function. Read:
    I may as well malloc then free myself. Worst yet, not only that, but
    since it's a stack, it pops off the very last object added to the
    stack... so if I allocate the temporary string and return it, then pop
    it off the stack later, it will free that string even though it's in
    use.
    
    I'll have to put this off to the side for now, very interesting
    though. Maybe, no, definitely after I actually finish String_Utils,
    Networking_Utils and File_Utils, AND Data_Structures (and Algorithms?)
    as I already have a huge amount on my plate. Much later.

---
+++ Development/String_Utils/Code/String_Utils_Garbage_Collector.c
@@ -0,0 +1,99 @@
+#include "String_Utils_Garbage_Collector.h"
+
+
+/* Virtual Machine constructor */
+SU_VM *SU_VM_Create(){
+	SU_VM *vm = malloc(sizeof(SU_VM));
+	vm->stack_size = 0;
+	vm->first = NULL; // Initialize first node to null.
+	vm->number_of_strings = 0;
+	vm->max_to_trigger = AMOUNT_TO_TRIGGER_GC;
+	return vm;
+}
+
+/* Pushes the String object onto the stack. */
+void push(SU_VM *vm, String_t *string){
+	assert(vm->stack_size < STACK_MAX, "Stack Overflow!\n");
+	vm->stack[vm->stack_size++] = string;
+}
+
+/* Pops the String object off the stack. */
+String_T *pop(SU_VM *vm){
+	assert(vm->stack_size > 0, "Stack Underflow!\n");
+	return vm->stack[--vm->stack_size];
+}
+
+/* Constructor for the String Object */
+String_T *String_Create(SU_VM *vm, String_Type type){
+	if(vm->number_of_strings == vm->max_to_trigger) SU_GC(vm);
+	String_T *string = malloc(sizeof(String_T));
+	string->type = type;
+	string->marked = 0;
+	string->next = vm->firstObject;
+	vm->first = string; // Since it's a stack, the first is always the newest one.
+	vm->number_of_strings++;
+	return string;
+}
+
+/* Push the passed string on the stack. */
+void push_string(SU_VM *vm, const char *str){
+	/* Initialized relevant fields in String_T */
+	String_T *string = String_Create(vm, STRING);
+	string->string = str;
+	/* Push newly initialized String_T on stack */
+	push(vm, string);
+}
+
+/* Push the array of strings and it's size on the stack. */
+/* void push_string_array(SU_VM *vm, const char **array_of_strings, size_t *size){
+	// Initialize relevant fields in String_T
+	String_T *string = String_Create(vm, STRING_ARRAY);
+	string->array_of_strings = array_of_strings;
+	string->size = size;
+	push(vm, string);
+} */
+
+/* Marks all String_T objects in the stack (virtual machine) */
+void markAll(SU_VM *vm){
+	int i = 0;
+	for(i; i < vm->stack_size; i++){
+		mark(vm->stack[i]);
+	}
+}
+
+/* Marks the passed string as still referenced (A.K.A Do not collect) */
+void mark(String_T *string){
+	if(string->marked) return; // If already marked return.
+	string->marked = 1;
+}
+
+void sweep(SU_VM *vm){
+	String_T **string = &vm->firstObject; // Array of string_t objects.
+	while(*string){ // While the current string is not NULL
+		if(!(*string)->marked) { // If the current string is not marked...
+			String_T *unreached = *string; // No longer contains a reference to it.
+			*string = unreached->next; // Sets the current string to the next node.
+			free(unreached->string); // Free the object's string.
+			free(unreached); // Free this string object.
+		} else {
+			// Of course, if it's still marked, it's still in used. So we unmark it for the next 
+			// Garbage collection cycle.
+			(*string)->marked = 0; // Unmarks current string
+			string = &(*string)->next; // Moves array up to the next index.
+		}
+	}
+}
+
+void SU_GC(SU_VM *vm){
+	int number_of_strings = vm->number_of_strings;
+	markAll(vm);
+	sweep(vm);
+	vm->max_to_trigger = vm->number_of_strings * 2;
+	printf("Collected %d objects, %d remaining.\n", number_of_strings - vm->number_of_strings, vm->number_of_strings);
+}
+
+void SU_VM_Destroy(SU_VM, *vm){
+	vm->stack_size = 0;
+	gc(vm);
+	free(vm);
+}
---
+++ Development/String_Utils/Code/String_Utils_Garbage_Collector.h
@@ -0,0 +1,50 @@
+#ifndef SU_GC_H
+#define SU_GC_H
+
+typedef struct String_Pointer {
+	struct String_Pointer *next; // Node that points to the next object.
+	unsigned char marked; // Marked bit
+	char *string; // The string to be stored when allocated.
+} String_T;
+
+#define MAX_SIZE 1024
+#define AMOUNT_TO_TRIGGER_GC 4
+
+/* Struct to hold the objects allocated on the stack. */
+typedef struct {
+	String_T *first; // The pointer to the very first node in the stack.
+	String_T *stack[MAX_SIZE]; // The stack of all allocated String_T
+	int number_of_strings; // Current number of strings allocated on the stack.
+	int max_to_trigger; // The number needed to trigger the Garbage Collector.
+	int stack_size;
+} SU_VM; // String_Utils_Virtual_Machine
+
+/* Virtual Machine constructor */
+SU_VM *SU_VM_Create();
+
+/* Pushes the String object onto the stack. */
+void push(SU_VM *vm, String_t *string);
+
+/* Pops the String object off the stack. */
+String_T *pop(SU_VM *vm);
+
+/* Constructor for the String Object */
+String_T *String_Create(SU_VM *vm, String_Type type);
+
+/* Push the passed string on the stack. */
+void push_string(SU_VM *vm, const char *str);
+
+/* Marks all String_T objects in the stack (virtual machine) */
+void markAll(SU_VM *vm);
+
+/* Marks the passed string as still referenced (A.K.A Do not collect) */
+void mark(String_T *string);
+
+/* Goes through the linked list of allocated String_T's, and will free what is not marked. */
+void sweep(SU_VM *vm);
+
+/* Garbage Collector for String_Utils */
+void SU_GC(SU_VM *vm);
+
+void SU_VM_Destroy(SU_VM, *vm);
+#endif /* SU_GC_H */
