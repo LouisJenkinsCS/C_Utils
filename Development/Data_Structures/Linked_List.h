7b6bc61f905a3fdcad065175e319cf4b48e3316e
Louis Jenkins <theif517@gmail.com>
2015-04-21 18:38:39 -0400

    Started on the template for my Linked List. However, this Linked List is no ordinary Linked List! This Linked List will be the base for any other Linked List based implementations I'll be doing. I already see it now.
    The Linked List is unimplemented, but I am working on the header files
    and brainstorming as to how I should create it. When finished, it should
    be generic enough to directly use with little modification for anything.
    It has a barebones iterator (struct), the Linked List (struct) has basic
    information like it's current size, the pointer to the first and last
    nodes, and even doubles as a single and double linked list through a
    tagged union.
    
    Now, how exactly is this going to work for any data structure I attempt
    to implement? Well for one, it has the option of passing in callback
    functions in the constructor, which if set will allow for custom
    operations, I.E Delete (Can't really fully delete a nested void * if I
    don't know in advance what's inside it now do I?), Add (In the case that
    special insertions are needed, like a binary tree), and Compare (For the
    eventual sorting function I'll implement, most likely merge sort). What
    makes it even more special, is that these aren't needed at all. If you
    pass in NULL for those parameters, the default, generic implementations
    of them will be used instead, making using it for simple applications
    viable.
    
    My mindset right now is to do all of the hard work now so I don't have
    to do it later. An extreme of DRY, a naive outlook of YAGNI (who
    wouldn't need a fully generic linked list?), and a somewhat lack of KISS
    (It's not overly complicated, but it needs to be some complex for what
    I'm trying to do). Wish me luck!

---
+++ Development/Data_Structures/Linked_List.h
@@ -0,0 +1,152 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+
+/* Typedef for the structs */
+typedef struct Node Node;
+typedef struct Linked_List Linked_List;
+typedef struct Iterator Iterator;
+typedef enum Node_T Node_T;
+
+/* Typedef for Linked_List function pointers */
+typedef int (*Linked_List_Add_Callback)(Linked_List *this, void *item);
+typedef int (*Linked_List_Delete_Callback)(Linked_List *this, Node *node);
+typedef int (*Linked_List_Compare_Callback)(Linked_List *this, void *item_one, void *item_two);
+
+
+/* Typedef for Iterator function pointers */
+/// Gets the next item... unfortunately, can't forward the node to the next.
+/// #define Iterator_Get_Current(this, type)((type *)((this)->(current)->item))
+
+typedef void *(*Iterator_get_next)(Iterator *iterator));
+typedef void *(*Iterator_get_previous)(Iterator *iterator));
+typedef void *(*Iterator_get_last)(Iterator *iterator));
+typedef void *(*Iterator_get_first)(Iterator *iterator));
+
+typedef int (*Iterator_delete_current)(Iterator *iterator);
+typedef int (*Iterator_append_current)(Iterator *iterator, void *item);
+typedef int (*Iterator_prepend_current)(Iterator *iterator, void *item);
+
+/* The struct that holds the next and previous pointers. The type of node,
+   A.K.A Node_T, will determine which struct in the union will be used. */
+struct Node {
+	void *item;
+	union {
+		/// For SINGLE Node_T
+		struct {
+			Node *single_next;
+		};
+		/// For DOUBLE Node_T
+		struct{
+			Node *double_next;
+			Node *double_prev;
+		};
+	};
+};
+
+/* Keep track of the only two possible types. */
+enum Node_T {
+	/// Obviously, for singly linked lists.
+	SINGLE,
+	/// For doubly linked lists.
+	DOUBLE
+}
+
+/* The Linked List structure which holds the basic information, the first and last node, and
+   some callback functions which are to be used to enforce polymorphism. */
+struct Linked_List{
+	/// The very first node.
+	Node *first;
+	/// The very last node.
+	Node *last;
+	/// The current size of the linked list.
+	size_t size;
+	/// Type of node
+	Node_T node_type;
+	/// Add callback.
+	Linked_List_Add_Callback add;
+	/// Delete callback.
+	Linked_List_Delete_Callback del;
+	/// Comparator callback.
+	Linked_List_Compare_Callback cmp;
+};
+
+/* A barebones iterator */
+struct Iterator {
+	/// The linked list to iterate through.
+	Linked_List *list;
+	/// The current node in the list the iterator is on.
+	Node *current;
+	/// Callback to get next
+	Iterator_get_next next;
+	/// Callback to get previous (only if doubly linked list!)
+	Iterator_get_previous prev;
+	/// Callback to get the last node.
+	Iterator_get_last last last;
+	/// Callback to get first node.
+	Iterator_get_first first first;
+	/// Callback to delete the current node.
+	Iterator_delete_current del;
+	/// Callback to append an item at the current index.
+	Iterator_append_current append;
+	/// Callback to prepend an item at the current index.
+	Iterator_prepend_current prepend;
+};
+
+/* Create the Linked List with the the supplied callbacks. If NULL is passed for any but Node_T, the default implementation 
+   for the callback will be used in it's place. */
+Linked_List *Linked_List_create(Linked_List_Add_Callback add, Linked_List_Delete_Callback del, Linked_List_Compare_Callback cmp, Node_T node_type);
+
+/* The standard, default callback used in place of a null Add_Callback parameter. */
+int Linked_List_default_add(Linked_List *this, void *item);
+
+/* The standard, default callback in place of a null Delete_Callback parameter */
+int Linked_List_default_delete(Linked_List *this, Node *node);
+
+/* The standard, default callback in place of a null Delete_Callback parameter */
+int Linked_List_default_compare(Linked_List *this, void *item_one, void *item_two);
+
+/* Returns an iterator for the linked list. This function will also set the required callbacks as well. */
+Iterator *Linked_List_get_iterator(Linked_List *this);
+
+/* Returns the next object in the iterator. */
+void * Iterator_get_next(Iterator *iterator);
+
+/* Returns the previous entry in the iterator if and only if it is a double linked list. */
+void * Iterator_get_previous(Iterator *iterator);
+
+/* A no-operation function that gets set on the previous callback if it is a single linked list. Returns NULL. */
+void * Iterator_nop(Iterator *iterator);
+
+/* Moves the current node to be the last, if it isn't already. */
+void * Iterator_get_last(Iterator *iterator);
+
+/* Moves the current node to be the first, if it isn't already. */
+void * Iterator_get_first(Iterator *iterator);
+
+/* Deletes the current node from the list. */
+int Iterator_delete_current(Iterator *iterator);
+
+/* Appends the item to the current node (as a node) */
+int Iterator_append_current(Iterator *iterator, void *item);
+
+/* Prepends the item to the current node (as a node) */
+int Iterator_prepend_current(Iterator *iterator, void *item);
+
+/* Deletes this iterator. Does not destroy the linked list along with it. */
+void Iterator_destroy(Iterator *iterator);
+
+/* Destroys the linked list along with all of it's contents. Make sure you get everything from the linked list before
+   calling this! */
+void Linked_List_destroy(Linked_List *list);
+
+
+
+
+
+
+
+
+
+
+#endif /* LINKED_LIST_H */
