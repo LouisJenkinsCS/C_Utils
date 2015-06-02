0346d09c5c95ea0ebfcc0738851d7fd68fc9329d
Louis Jenkins <theif517@gmail.com>
2015-06-02 16:10:42 -0400

    Completed the logging and timer functions, they work fine so far, have to test it out though, going to attempt to run my Thread Pool tests with it.

--- Development/Misc_Utils/Misc_Utils.c
+++ Development/Misc_Utils/Misc_Utils.c
@@ -1,25 +1,56 @@
 #include "Misc_Utils.h"
-#include <time.h>
+#include <stdlib.h>
 
 
 char *Misc_Utils_Get_Timestamp(void){
-	time_t current_time = time(NULL);
-	const int seconds_in_half_day = 43200;
+	const int buffer_size = 80;
+	time_t t = time(NULL);
+	struct tm *current_time = localtime(&t);
+	char *time_and_date = malloc(buffer_size);
+	strftime(time_and_date, buffer_size, "%I:%M:%S %p", current_time);
+	return time_and_date;
+}
+
+Timer_t *Timer_Init(int start){
+	Timer_t *timer = calloc(1, sizeof(Timer_t));
+	timer->start = calloc(1, sizeof(time_t));
+	timer->end = calloc(1 , sizeof(time_t));
+	if(start) time(timer->start);
+	return timer;
+}
+
+int Timer_Start(Timer_t *timer){
+	time(timer->start);
+	return 1;
+}
+
+int Timer_Stop(Timer_t *timer){
+	time(timer->end);
+	return 1;
+}
+
+char *Timer_To_String(Timer_t *timer){
+	double total_time = difftime(*timer->end, *timer->start);
 	int hours = 0, minutes = 0, seconds = 0;
-	char[2] meridiem = current_time >= seconds_in_half_day ? "PM" : "AM";
-	while((int)(current_time/3600)){
+	while((int)(total_time/3600)){
 		hours++;
 		total_time -= 3600;
 	}
-	while((int)(current_time/60)){
+	while((int)(total_time/60)){
 		minutes++;
 		total_time -= 60;
 	}
-	while((int)current_time){
+	while((int)total_time){
 		seconds++;
 		total_time--;
 	}
 	char *formatted_time;
-	asprintf(&formatted_time, "%02d:%02d:%02d %s", hours, minutes, seconds, meridiem);
+	asprintf(&formatted_time, "%02d:%02d:%02d", hours, minutes, seconds);
 	return formatted_time;
+}
+
+void Timer_Destroy(Timer_t *timer){
+	free(timer->start);
+	free(timer->end);
+	free(timer);
 }
--- Development/Misc_Utils/Misc_Utils.h
+++ Development/Misc_Utils/Misc_Utils.h
@@ -2,17 +2,68 @@
 #define MISC_UTILS_H
 
 #include <stdio.h>
-#ifdef MU_NO_DEBUG
+#include <time.h>
+
+#ifdef NDEBUG
+/// If NDEBUG is defined, then MU_DEBUG becomes a NOP.
 #define MU_DEBUG(message, ...)
 #else
-#define MU_DEBUG(message, ...) fprintf(stderr, "[DEBUG](%s:%d): '" message "'\n", __FILE__, __LINE__, ##__VA_ARGS__)
+/// Prints a debug message, along with it's timestamp, file and line of code it's on.
+#define MU_DEBUG(message, ...) do { \
+	char *timestamp = Misc_Utils_Get_Timestamp(); \
+	fprintf(stderr, "%s: [DEBUG](%s:%d): '" message "'\n", timestamp,  __FILE__, __LINE__, ##__VA_ARGS__); \
+	free(timestamp); \
+} while(0)
 #endif
+/// An assertion which prints to stderr, the logfile and also shows the file and line that triggered it as well as timestamp.
+#define MU_ASSERT(condition, file) do { \
+	if(!(condition)){ \
+		char *timestamp = Misc_Utils_Get_Timestamp(); \
+		fprintf(stderr, "Assertion Failed! See log!\n"); \
+		fprintf(file, "%s: [ASSERT](%s:%d) An Assertion for '" #condition "' has failed!\n", Misc_Utils_Get_Timestamp(), __FILE__, __LINE__); \
+		free(timestamp); \
+		exit(EXIT_FAILURE); \
+	} \
+} while(0)
+/// Log an error message along with timestamp, file and line of code.
+#define MU_LOG_ERROR(file, message, ...) do { \
+	char *timestamp = Misc_Utils_Get_Timestamp(); \
+	fprintf(file, "%s: [ERROR](%s:%d) " message "\n", timestamp, __FILE__, __LINE__, ##__VA_ARGS__); \
+	free(timestamp); \
+} while(0) 
+/// Log a warning message along with timestamp, file and line of code.
+#define MU_LOG_WARNING(file, message, ...) do { \
+	char *timestamp = Misc_Utils_Get_Timestamp(); \
+	fprintf(file, "%s: [WARNING](%s:%d) " message "\n", timestamp, __FILE__, __LINE__, ##__VA_ARGS__); \
+	free(timestamp); \
+} while(0)
+/// Log an info message along with timestamp, file and line of code.
+#define MU_LOG_INFO(file, message, ...) do { \
+	char *timestamp = Misc_Utils_Get_Timestamp(); \
+	fprintf(file, "%s: [INFO](%s:%d) " message "\n", timestamp, __FILE__, __LINE__, ##__VA_ARGS__); \
+	free(timestamp); \
+} while(0)
+/// Checks if a variable is true, otherwise will return. Will not log to file.
+#define MU_CHECK(obj, retval, message, ...) do { \
+	if(!(obj)) MU_DEBUG(message, ##__VA_ARGS__); \
+	return retval; \
+} while(0)
 
-#define MU_ASSERT(condition, file)(!condition ? fprintf(file, "%s: [ASSERT](%s:%d) An Assertion for '" condition "' has failed!\n", Misc_Utils_Get_Timestamp __FILE__, __LINE__) : condition)
-#define MU_LOG_ERROR(file, message, ...) fprintf(file, "%s: [ERROR](%s:%d) " message "\n", Misc_Utils_Get_Timestamp, __FILE__, __LINE__, ##__VA_ARGS__)
-#define MU_LOG_WARNING(file, message, ...) fprintf(file, "%s: [WARNING](%s:%d) " message "\n", Misc_Utils_Get_Timestamp, __FILE__, __LINE__, ##__VA_ARGS__)
-#define MU_LOG_INFO(file, message, ...) fprintf(file, "%s: [INFO](%s:%d) " message "\n", Misc_Utils_Get_Timestamp, __FILE__, __LINE__, ##__VA_ARGS__)
-#define MU_CHECK(obj, message, ...)(!obj ? MU_DEBUG(message, ##__VA_ARGS__) : obj)
+typedef struct {
+	time_t *start;
+	time_t *end;
+} Timer_t;
 
+Timer_t *Timer_Init(int start);
+
+int Timer_Start(Timer_t *timer);
+
+int Timer_Stop(Timer_t *timer);
+
+char *Timer_To_String(Timer_t *timer);
+
+void Timer_Destroy(Timer_t *timer);
+
+/// Used to obtain a timestamp.
 char *Misc_Utils_Get_Timestamp(void);
 #endif
