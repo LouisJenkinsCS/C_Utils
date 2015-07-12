4bbc9591a85c61551738e8c2324e54ea2d065030
theif519 <theif517@gmail.com>
2015-07-12 16:39:43 -0400

    Refactored all tests, now it's ready to run to see if it works.

--- Net_Utils/Tests/NU_Client_Test_File_Downloader.c
+++ Net_Utils/Tests/NU_Client_Test_File_Downloader.c
@@ -12,11 +12,11 @@ static const unsigned int port_num = 10000;
 
 static const char *assert_get_info(NU_Server_Socket_t *server, const char *inquiry){
   size_t bytes_sent = NU_Client_send(client, server, (const char *)inquiry, strlen(inquiry), timeout);
-  MU_ASSERT((bytes_sent == strlen(inquiry)), logger,
-	    "Not all bytes were sent to client!\nbytes_sent: %zu, message_bytes: %zu", bytes_sent, strlen(inquiry));
-  const char *retval = NU_Client_receive(client, server, buffer_size, timeout);
-  MU_ASSERT(retval, logger, "Client either disconnected or did not send result in time!");
-  return retval;
+  MU_ASSERT(bytes_sent, logger, "Was unable to send information to server!\n");
+  char *response = calloc(buffer_size + 1);
+  size_t bytes_received = NU_Client_receive(client, server, response, buffer_size, timeout);
+  MU_ASSERT(bytes_received, logger, "Was unable to get response from server!\n");
+  return response;
 }
 
 static void send_file(NU_Server_Socket_t *server){
@@ -25,13 +25,13 @@ static void send_file(NU_Server_Socket_t *server){
   asprintf(&filepath, "%s/%s/%s", getenv("HOME"), "Pictures", filename);
   MU_DEBUG("Opening \"%s\"\n", filepath);
   FILE *file = fopen(filepath, "rb");
-  MU_ASSERT(file, logger, "Was unable to open file: \"%s\" with mode \"%s\"", filepath, "rb");
-  size_t retval = NU_Client_send(client, server, filename, strlen(filename), timeout);
+  MU_ASSERT(file, logger, "Was unable to open file: \"%s\" with mode \"%s\"!\nfopen: \"%s\"\n", filepath, "rb", strerror(errno));
+  size_t sent = NU_Client_send(client, server, filename, strlen(filename), timeout);
   MU_DEBUG("Sent filename \"%s\"", filename);
-  MU_ASSERT(retval, logger, "Was unable to send filename to server!\n");
+  MU_ASSERT(sent, logger, "Was unable to send filename to server!\n");
   //MU_DEBUG("File Path:");
   //MU_ASSERT(fgets(filepath, 255, stdin), logger, "Invalid input from user!\n");
-  retval = NU_Client_send_file(client, server, file, buffer_size, 1, timeout);
+  sent = NU_Client_send_file(client, server, file, buffer_size, timeout);
   MU_DEBUG("Sent %zu bytes to server!\n", retval);
   MU_ASSERT(retval, logger, "Was unable to send file to server!\n");
 }
@@ -41,7 +41,7 @@ int main(void){
   char *client_inquiry;
   logger = calloc(1, sizeof(MU_Logger_t));
   MU_Logger_Init(logger, "NU_Client_Test_File_Downloader_Log.txt", "w", MU_ALL);
-  client = NU_Client_create(0);
+  client = NU_Client_create(1);
   MU_ASSERT(client, logger, "Was unable to create client!\n");
   //char *ip_addr = calloc(1, INET_ADDRSTRLEN);
   //MU_DEBUG("IP Address:");
@@ -53,8 +53,8 @@ int main(void){
   char *filename = "kitten.jpg";
   char *mode = "wb";
   FILE *file = fopen(filename, mode);
-  MU_ASSERT(file, logger, "Was unable to create file \"%s\" with mode \"%s\"\n", filename, mode); 
-  size_t retval = NU_Client_receive_to_file(client, server, file, buffer_size, 1, timeout);
-  MU_ASSERT(retval, logger, "Client was unable to receive file from server!\n");
-  NU_Client_destroy(client, "Shutting down!\n");
+  MU_ASSERT(file, logger, "Was unable to create file \"%s\" with mode \"%s\"\nfopen: \"%s\"\n", filename, mode, strerror(errno)); 
+  size_t received = NU_Client_receive_to_file(client, server, file, buffer_size, 1, timeout);
+  MU_ASSERT(received, logger, "Client was unable to receive file from server!\n");
+  NU_Client_destroy(client);
 }
--- Net_Utils/Tests/NU_Server_Test_Client_Chat.c
+++ Net_Utils/Tests/NU_Server_Test_Client_Chat.c
@@ -5,23 +5,23 @@
 
 static MU_Logger_t *logger = NULL;
 
-NU_Server_t *server = NULL;
+static NU_Server_t *server = NULL;
 
 static const int username_max_length = 16;
 static const unsigned int avg_timeout = 30;
 static const unsigned int generous_timeout = 60;
 static const unsigned int port_num = 10000;
-static const unsigned int queue_max = 2;
+static const unsigned int max_clients = 2;
 
 typedef struct {
-	NU_Client_Socket_t *client;
-	char *username;
+	NU_Connection_t *client;
+	char username[username_max_length];
 } Client_Wrapper_t;
 
-static Client_Wrapper_t *wrap_client(NU_Client_Socket_t *client, char *username){
+static Client_Wrapper_t *wrap_client(NU_Connection_t *client, char *username){
 	Client_Wrapper_t *wrapper = calloc(1, sizeof(Client_Wrapper_t));
 	wrapper->client = client;
-	wrapper->username = username;
+	strcpy(wrapper->username, username);
 	return wrapper;
 }
 
@@ -50,66 +50,67 @@ static void *redirector_thread(void *args){
 	strcpy(username_prefix, task->sender->username);
 	strcat(username_prefix, prefix);
 	username_prefix[username_prefix_size] = '\0';
+	char message[task->buffer_size + 1];
+	char redirect_message[task->buffer_size + username_prefix_size + 1];
 	while(1){
-		char *msg = strdup(NU_Server_receive(server, task->sender->client, task->buffer_size, task->timeout));
-		if(!msg) break;
-		strtok(msg, "\r\n");
-		size_t new_msg_size = strlen(username_prefix) + strlen(msg) + strlen(suffix);
-		char *new_msg = calloc(1, new_msg_size + 1);
-		strcpy(new_msg, username_prefix);
-		strcat(new_msg, msg);
-		strcat(new_msg, suffix);
-		new_msg[new_msg_size] = '\0';
-		MU_LOG_VERBOSE(logger, "%s\n", new_msg);
-		if(!NU_Server_send(server, task->receiver->client, (const char *)new_msg, task->timeout)) {
-			free(new_msg);
+		size_t received = NU_Server_receive(server, task->sender->client, msg, task->buffer_size, task->timeout));
+		if(!received){
 			break;
 		}
-		free(msg);
-		free(new_msg);
+		msg[received] = '\0';
+		strtok(msg, "\r\n");
+		strcpy(redirect_message, username_prefix);
+		strcat(redirect_message, msg);
+		strcat(redirect_message, suffix);
+		//MU_LOG_VERBOSE(logger, "%s\n", new_msg);
+		if(!NU_Server_send(server, task->receiver->client, (const char *)redirect_message, strlen(redirect_message), task->timeout)) {
+			break;
+		}
 	}
-	char *end_msg;
-	asprintf(&end_msg, "%s: Shutting down!\n", task->sender->username);
-	NU_Server_disconnect(server, task->sender->client, (const char *)end_msg);
-	free(task->sender->username);
+	NU_Server_disconnect(server, task->sender->client);
 	free(task);
 	return NULL;
 }
 
 static Client_Wrapper_t *obtain_client(NU_Bound_Socket_t *bsock){
-	NU_Client_Socket_t *client = NU_Server_accept(server, bsock, 60);
+	NU_Connection_t *client = NU_Server_accept(server, bsock, generous_timeout);
 	MU_ASSERT(client, logger, "Client did not connect in time!\n");
-	MU_ASSERT(NU_Server_send(server, client, "Username:", avg_timeout), logger,
-		"Client timed out or disconnected while asking for username!\n");
-	char *username = strdup(NU_Server_receive(server, client, username_max_length, avg_timeout));
-	MU_ASSERT(username, logger, "Client timed out or disconnected waiting to obtain username!\n");
+	char username[username_max_length];
+	size_t sent = NU_Server_send(server, client, "Username:", strlen("Username:") avg_timeout);
+	MU_ASSERT(sent, logger, "Client timed out or disconnected while asking for username!\n");
+	size_t received = NU_Server_receive(server, client, username, username_max_length, avg_timeout);
+	MU_ASSERT(received, logger, "Client timed out or disconnected waiting to obtain username!\n");
 	strtok(username, "\r\n");
 	return wrap_client(client, username);
 }
 
 int main(void){
 	logger = calloc(1, sizeof(MU_Logger_t));
-	MU_Logger_Init(logger, "NU_Server_Test_Log.txt", "w", MU_ALL);
-	server = NU_Server_create(0);
-	NU_Bound_Socket_t *bsock = NU_Server_bind(server, "10.0.2.15", port_num, queue_max, NU_NONE);
+	MU_Logger_Init(logger, "NU_Server_Test.log", "w", MU_ALL);
+	server = NU_Server_create(max_clients, 1);
+	NU_Bound_Socket_t *bsock = NU_Server_bind(server, "10.0.2.15", port_num, max_clients, NU_NONE);
 	MU_ASSERT(bsock, logger, "Failed while attempting to bind a socket!");
 	pthread_t thread_one, thread_two;
-	Client_Wrapper_t **wrappers = malloc(sizeof(Client_Wrapper_t *) * queue_max);
+	Client_Wrapper_t **wrappers = malloc(sizeof(Client_Wrapper_t *) * max_clients);
 	int i = 0;
-	for(;i < queue_max; i++){
+	for(;i < max_clients; i++){
 		MU_DEBUG("Accepting a new client...\n");
 		Client_Wrapper_t *wrapper = obtain_client(bsock);
 		MU_ASSERT(wrapper, logger, "Failed to obtain wrapper for client socket!\n");
 		wrappers[i] = wrapper;
 		MU_DEBUG("Connected to %s!\n", wrapper->username);
 	}
-	Thread_Task_t *wrapper_one_task = new_task(wrappers[0], wrappers[1], 1024 * 4, avg_timeout);
-	Thread_Task_t *wrapper_two_task = new_task(wrappers[1], wrappers[0], 1024 * 4, avg_timeout);
-	MU_ASSERT(!pthread_create(&thread_one, NULL, redirector_thread, wrapper_one_task), logger, "pthread_create: \"%s\"\n", strerror(errno));
-	MU_ASSERT(!pthread_create(&thread_two, NULL, redirector_thread, wrapper_two_task), logger, "pthread_create: \"%s\"\n", strerror(errno));
+	Thread_Task_t *wrapper_one_task = new_task(wrappers[0], wrappers[1], 4 * NU_KILOBYTE, avg_timeout);
+	Thread_Task_t *wrapper_two_task = new_task(wrappers[1], wrappers[0], 4 * NU_KILOBYTE, avg_timeout);
+	int created_thread = pthread_create(&thread_one, NULL, redirector_thread, wrapper_one_task);
+	MU_ASSERT(created_thread, logger, "pthread_create: \"%s\"\n", strerror(created_thread));
+	created_thread = pthread_create(&thread_two, NULL, redirector_thread, wrapper_two_task);
+	MU_ASSERT(created_thread, logger, "pthread_create: \"%s\"\n", strerror(created_thread));
 	MU_DEBUG("Threads started...\n");
-	pthread_join(thread_one, NULL);
-	pthread_join(thread_two, NULL);
+	int joined_thread = pthread_join(thread_one, NULL);
+	MU_ASSERT(joined_thread, logger, "pthread_join: \"%s\"\n", strerror(joined_thread));
+	joined_thread = pthread_join(thread_two, NULL);
+	MU_ASSERT(joined_thread, logger, "pthread_join: \"%s\"\n", strerror(joined_thread));
 	char *end_result = NU_Server_about(server);
 	MU_DEBUG("%s\n", end_result);
 	free(end_result);
