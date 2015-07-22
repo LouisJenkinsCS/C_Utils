c6645e43dd530c98cb81caa3af75bffe0145873d
Louis Jenkins <theif519@gmail.com>
2015-07-22 02:53:23 -0400

    YAY! It works! Finally, I figured out a somewhat decent, although not quite elegant solution! I pass an array (allocated on stack, so it gets cleaned up at least) to the validating function to fill out to test for which pass and which fail. Although note to self that I need to work more on it as it looks ugly, but overall, decent.

--- Misc_Utils/MU_Arg_Check.c
+++ Misc_Utils/MU_Arg_Check.c
@@ -1,27 +1,20 @@
 #include <MU_Arg_Check.h>
 
-void MU_Arg_append_to_string(bool cond, char *cond_str, char **string_ptr){
-	char *tmp_str;
-	asprintf(&tmp_str, ";%s: %s", cond_str, MU_ARG_EVAL(cond));
-	char *old_string_ptr = *string_ptr;
-	asprintf(string_ptr, "%s%s", *string_ptr, tmp_str);
-	free(old_string_ptr);
-}
-
-bool MU_Arg_evaluate_arguments(int num_args, ...){
+bool MU_Arg_evaluate_arguments(int num_args, bool *arr, ...){
 	int i = 0;
 	va_list list;
-	va_start(list, num_args);
+	va_start(list, arr);
 	bool result = true;
 	for(; i < num_args; i++){
-		if(!va_arg(list, int)){
+		bool is_valid = va_arg(list, int);
+		arr[i] = is_valid;
+		if(!is_valid){
 			result = false;
-			break;
 		}
 	}
 	return result;
 }
 
-bool MU_Arg_to_bool(void *arg){
-	return arg;
+char *MU_Arg_evaluate_boolean(bool arg){
+	return arg ? "TRUE" : "FALSE";
 }
--- Misc_Utils/MU_Arg_Check.h
+++ Misc_Utils/MU_Arg_Check.h
@@ -8,23 +8,25 @@
 /// Genius implementation someone else made to find the amount of variadic functions.
 #define MU_ARG_COUNT(...) (sizeof((bool[]){__VA_ARGS__})/sizeof(bool))
 
-#define MU_ARG_EVAL(arg) (arg ? "TRUE" : "FALSE")
-
 #define MU_ARG_TO_STRING(arg)("" #arg "")
 
 #define MU_ARG_CHECK(logger, retval, ...) do { \
 	int num_args = MU_ARG_COUNT(__VA_ARGS__); \
-	if(!MU_Arg_evaluate_arguments(num_args, __VA_ARGS__)) break; \
+	bool valid_args[8]; \
+	if(MU_Arg_evaluate_arguments(num_args, valid_args, __VA_ARGS__)) break; \
 	char *arg_str = calloc(1, 512); \
 	sprintf(arg_str, "%s: Invalid Argument%s=> \"", __FUNCTION__, num_args > 1 ? "s" : ""); \
-	if(num_args == 1) MU_ARG_CHECK_1(arg_str, __VA_ARGS__); \
-	else if(num_args == 2) MU_ARG_CHECK_2(arg_str, __VA_ARGS__); \
-	else if(num_args == 3) MU_ARG_CHECK_3(arg_str, __VA_ARGS__); \
-	else if(num_args == 4) MU_ARG_CHECK_4(arg_str, __VA_ARGS__); \
-	else if(num_args == 5) MU_ARG_CHECK_5(arg_str, __VA_ARGS__); \
-	else if(num_args == 6) MU_ARG_CHECK_6(arg_str, __VA_ARGS__); \
-	else if(num_args == 7) MU_ARG_CHECK_7(arg_str, __VA_ARGS__); \
-	else if(num_args == 8) MU_ARG_CHECK_8(arg_str, __VA_ARGS__); \
+	switch(num_args){ \
+		case 1: MU_ARG_CHECK_1(arg_str, valid_args, __VA_ARGS__); break; \
+		case 2: MU_ARG_CHECK_2(arg_str, valid_args, __VA_ARGS__); break; \
+		case 3: MU_ARG_CHECK_3(arg_str, valid_args, __VA_ARGS__); break; \
+		case 4: MU_ARG_CHECK_4(arg_str, valid_args, __VA_ARGS__); break; \
+		case 5: MU_ARG_CHECK_5(arg_str, valid_args, __VA_ARGS__); break; \
+		case 6: MU_ARG_CHECK_6(arg_str, valid_args, __VA_ARGS__); break; \
+		case 7: MU_ARG_CHECK_7(arg_str, valid_args, __VA_ARGS__); break; \
+		case 8: MU_ARG_CHECK_8(arg_str, valid_args, __VA_ARGS__); break; \
+		default: assert(0); \
+	} \
 	sprintf(arg_str, "%s!\"\n", arg_str); \
 	MU_LOG_ERROR(logger, "%s", arg_str); \
 	free(arg_str); \
@@ -32,20 +34,24 @@
 } while(0)
 
 
-#define MU_ARG_CHECK_8(string_ptr, arg, ...) sprintf(string_ptr, "%s;%s: %s", *string_ptr,  MU_ARG_TO_STRING(arg), MU_ARG_EVAL(arg)); MU_ARG_CHECK_7(string_ptr, __VA_ARGS__)
+#define MU_ARG_CHECK_8(string_ptr, valid_args, arg, ...) sprintf(string_ptr, "%s;%s: %s", string_ptr,  MU_ARG_TO_STRING(arg), MU_Arg_evaluate_boolean(valid_args[7])); MU_ARG_CHECK_7(string_ptr, valid_args, __VA_ARGS__)
 
-#define MU_ARG_CHECK_7(string_ptr, arg, ...) sprintf(string_ptr, "%s;%s: %s", *string_ptr,  MU_ARG_TO_STRING(arg), MU_ARG_EVAL(arg)); MU_ARG_CHECK_6(string_ptr, __VA_ARGS__)
+#define MU_ARG_CHECK_7(string_ptr, valid_args, arg, ...) sprintf(string_ptr, "%s;%s: %s", string_ptr,  MU_ARG_TO_STRING(arg), MU_Arg_evaluate_boolean(valid_args[6])); MU_ARG_CHECK_6(string_ptr, valid_args,  __VA_ARGS__)
 
-#define MU_ARG_CHECK_6(string_ptr, arg, ...) sprintf(string_ptr, "%s;%s: %s", *string_ptr,  MU_ARG_TO_STRING(arg), MU_ARG_EVAL(arg)); MU_ARG_CHECK_5(string_ptr, __VA_ARGS__)
+#define MU_ARG_CHECK_6(string_ptr, valid_args, arg, ...) sprintf(string_ptr, "%s;%s: %s", string_ptr,  MU_ARG_TO_STRING(arg), MU_Arg_evaluate_boolean(valid_args[5])); MU_ARG_CHECK_5(string_ptr, valid_args,  __VA_ARGS__)
 
-#define MU_ARG_CHECK_5(string_ptr, arg, ...) sprintf(string_ptr, "%s;%s: %s", *string_ptr,  MU_ARG_TO_STRING(arg), MU_ARG_EVAL(arg)); MU_ARG_CHECK_4(string_ptr, __VA_ARGS__)
+#define MU_ARG_CHECK_5(string_ptr, valid_args, arg, ...) sprintf(string_ptr, "%s;%s: %s", string_ptr,  MU_ARG_TO_STRING(arg), MU_Arg_evaluate_boolean(valid_args[4])); MU_ARG_CHECK_4(string_ptr, valid_args,  __VA_ARGS__)
 
-#define MU_ARG_CHECK_4(string_ptr, arg, ...) sprintf(string_ptr, "%s;%s: %s", *string_ptr,  MU_ARG_TO_STRING(arg), MU_ARG_EVAL(arg)); MU_ARG_CHECK_3(string_ptr, __VA_ARGS__)
+#define MU_ARG_CHECK_4(string_ptr, valid_args, arg, ...) sprintf(string_ptr, "%s;%s: %s", string_ptr,  MU_ARG_TO_STRING(arg), MU_Arg_evaluate_boolean(valid_args[3])); MU_ARG_CHECK_3(string_ptr, valid_args,  __VA_ARGS__)
 
-#define MU_ARG_CHECK_3(string_ptr, arg, ...) sprintf(string_ptr, "%s;%s: %s", *string_ptr,  MU_ARG_TO_STRING(arg), MU_ARG_EVAL(arg)); MU_ARG_CHECK_2(string_ptr, __VA_ARGS__)
+#define MU_ARG_CHECK_3(string_ptr, valid_args, arg, ...) sprintf(string_ptr, "%s;%s: %s", string_ptr,  MU_ARG_TO_STRING(arg), MU_Arg_evaluate_boolean(valid_args[2])); MU_ARG_CHECK_2(string_ptr, valid_args,  __VA_ARGS__)
 
-#define MU_ARG_CHECK_2(string_ptr, arg, ...) sprintf(string_ptr, "%s;%s: %s", *string_ptr,  MU_ARG_TO_STRING(arg), MU_ARG_EVAL(arg)); MU_ARG_CHECK_1(string_ptr, __VA_ARGS__)
+#define MU_ARG_CHECK_2(string_ptr, valid_args, arg, ...) sprintf(string_ptr, "%s;%s: %s", string_ptr,  MU_ARG_TO_STRING(arg), MU_Arg_evaluate_boolean(valid_args[1])); MU_ARG_CHECK_1(string_ptr, valid_args,  __VA_ARGS__)
 
-#define MU_ARG_CHECK_1(string_ptr, arg) sprintf(string_ptr, "%s;%s: %s", *string_ptr,  MU_ARG_TO_STRING(arg), MU_ARG_EVAL(arg));
+#define MU_ARG_CHECK_1(string_ptr, valid_args, arg, ...) sprintf(string_ptr, "%s;%s: %s", string_ptr,  MU_ARG_TO_STRING(arg), MU_Arg_evaluate_boolean(valid_args[0]));
+
+bool MU_Arg_evaluate_arguments(int num_args, bool *arr, ...);
+
+char *MU_Arg_evaluate_boolean(bool arg);
 
 #endif /* endif MU_ARG_CHECK_H */
